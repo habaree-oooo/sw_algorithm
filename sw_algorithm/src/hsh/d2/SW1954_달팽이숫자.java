8aa2c4cd030be51522f83b0930ec5bb7e313451c
habaree-oooo <45867621+habaree-oooo@users.noreply.github.com>
2019-08-05 21:16:21 +0900

    Add files via upload

---
+++ sw_algorithm/src/hsh/d2/SW1954_달팽이숫자.java
@@ -0,0 +1,23 @@
+package d2.sw1954;
+//https://froper.tistory.com/9
+import java.util.Scanner;
+
+public class SW1954_달팽이숫자 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tcn=1; tcn<=T; tcn++){
+			int N = sc.nextInt();
+			int[ ][ ]arr = new int[N][N];
+			
+			for(int ir=0; ir<N; ir++){
+				for(int ic = 0; ic<N; ic++){
+					
+				}
+			}
+		}
+
+	}
+
+}
